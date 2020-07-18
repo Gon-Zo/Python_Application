c00ca3d1425d14fb417948b22350b2f68ebd3c32
gonzo <m165252@daum.net>
2020-07-18 12:00:59 +0900

    변수 기초

---
+++ basic/venv/bin/app/variable/list.py
@@ -0,0 +1,13 @@
+# 리스트 형
+
+list = ['a', 'b', 'c']
+
+print(list)
+
+# 리스트 하나
+
+print(list[0])
+
+list.remove('a')
+
+print(list)
---
+++ basic/venv/bin/app/variable/variable.py
@@ -0,0 +1,11 @@
+# 정수 형
+num = 100
+
+print(num)
+
+# 문자 형
+
+str = "나는 문자"
+
+print(str)
+
