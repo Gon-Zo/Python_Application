cee3627387d8f13a3b9064565b00653164c2a9e5
gonzo <m165252@daum.net>
2020-07-18 12:26:30 +0900

    반복문 for문 & range 함수

---
+++ basic/venv/bin/app/for/for.py
@@ -0,0 +1,4 @@
+list = ['a' , 'b' , 'c' , 'd' , 'e']
+
+for item in list:
+    print("List 값=%s List 길이=%d" % (item, len(item)))
---
+++ basic/venv/bin/app/for/range.py
@@ -0,0 +1,8 @@
+# list 에 2 부터 7 까지 담는다
+
+array = list(range(2, 7))
+
+print(array)
+
+for val in array:
+    print(val)
