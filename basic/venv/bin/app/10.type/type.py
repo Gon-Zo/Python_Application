2b4c59eb022abae6ba154a98a1aec2d446051282
gonzo <m165252@daum.net>
2020-07-19 19:18:53 +0900

    변수의 타입

---
+++ basic/venv/bin/app/10.type/type.py
@@ -0,0 +1,26 @@
+# Type
+
+# 문자열
+a = type('a')
+
+print(a)
+
+# 정수
+b = type(1)
+
+print(b)
+
+# 소수
+c = type(1.1)
+
+print(c)
+
+#  복소수
+d = type(3 + 4j)
+
+print(d)
+
+# 리스트
+f = type([1, 2, 3, 4, 5])
+
+print(f)
